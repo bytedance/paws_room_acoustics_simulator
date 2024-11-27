226ba4c1a42022e3822a6f81cab2bbe7d67936f1
Tamamo0v0 <61530409+Tamamo0v0@users.noreply.github.com>
2024-11-27 19:50:51 +0800

    Delete unused function read_obj.py

--- paws/read_obj.py
+++ paws/read_obj.py
@@ -54,49 +54,7 @@ def read_obj(path,Nx,Ny,Nz,padding_coef,base_sound_speed,base_density,base_alpha
 
     return sound_speed_grid,density_grid,alpha_grid
     
-    
 
-# def read_obj_plan_b(path):
-    
-#     file_max_bound = []
-#     file_min_bound = []
-
-#     file_max_bound = [-math.inf]*3
-#     file_min_bound = [math.inf]*3
-
-#     mesh_list = []
-
-#     # find max and min bound
-#     for file in selected_file:
-
-#         filename = dir_path + "/" + file
-
-#         mesh = o3d.io.read_triangle_mesh(filename)
-        
-#         file_max_bound = np.max([mesh.get_max_bound(),file_max_bound],0)
-#         file_min_bound = np.min([mesh.get_min_bound(),file_min_bound],0)
-        
-        
-#     # transform the mesh into unit scale [-0.5,0.5]
-
-#     for file in selected_file:
-
-#         filename = dir_path + "/" + file
-
-#         mesh = o3d.io.read_triangle_mesh(filename)
-        
-        
-#         # resize the mesh into [-0.5,0.5] unit square
-
-#         mesh.translate(-(file_max_bound + file_min_bound)/2)
-
-#         mesh.scale(1 / (np.max(file_max_bound - file_min_bound)),center=[0,0,0])
-
-#         mesh_list.append(mesh)
-
-#         voxel_grid = o3d.geometry.VoxelGrid.create_from_triangle_mesh(mesh,
-#                                                                 voxel_size=0.05)
-#         o3d.visualization.draw_geometries([voxel_grid])
 
 #         #show all of them
 #         o3d.visualization.draw_geometries(mesh_list)
